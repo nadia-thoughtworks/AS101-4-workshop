d8478953f22e23ac333d21797f638cf11663169a
Nadia Arias <nadia.arias@thoughtworks.com>
2022-09-30 10:35:25 +1000

    use the right entrypoint file

--- .github/actions/prep/entrypoint.sh
+++ .github/actions/prep/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-echo $VAULT_PASS > /Users/nadia/ansible_pass.txt
-echo $(ansible-vault view --vault-password-file=/Users/nadia/ansible_pass.txt env_secrets)|xargs -n1 >> $GITHUB_ENV 
-rm /Users/nadia/ansible_pass.txt
+echo $VAULT_PASS > /vault_pass.txt
+echo $(ansible-vault view --vault-password-file=/vault_pass.txt env_secrets)|xargs -n1 >> $GITHUB_ENV
+rm /vault_pass.txt
