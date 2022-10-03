522f777f979093bef80d54e2335f10fb35eac251
Nadia Arias <nadia.arias@thoughtworks.com>
2022-10-04 08:24:49 +1000

    try masking before passing vars to env

--- .github/actions/prep/entrypoint.sh
+++ .github/actions/prep/entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 echo $VAULT_PASS > /vault_pass.txt
+echo "::add-mask::$POSTGRES_USER"
+echo "::add-mask::$POSTGRES_PASSWORD"
+echo "::add-mask::$POSTGRES_DB"
 echo $(ansible-vault view --vault-password-file=/vault_pass.txt env_secrets)|xargs -n1 >> $GITHUB_ENV
 rm /vault_pass.txt
