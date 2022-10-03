75028e9ebf7d12780aa18729925c62d2f418f800
Nadia Arias <nadia.arias@thoughtworks.com>
2022-10-04 09:02:02 +1000

    nope

--- .github/actions/prep/entrypoint.sh
+++ .github/actions/prep/entrypoint.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
 echo $VAULT_PASS > /vault_pass.txt
-echo $(ansible-vault view --vault-password-file=/vault_pass.txt env_secrets)|xargs -n1
-echo "::add-mask::$POSTGRES_USER"
-echo "::add-mask::$POSTGRES_PASSWORD"
-echo "::add-mask::$POSTGRES_DB"
-echo  POSTGRES_USER=$POSTGRES_USER >> $GITHUB_ENV
-echo  POSTGRES_PASSWORD=$POSTGRES_PASSWORD >> $GITHUB_ENV
-echo  POSTGRES_DB=$POSTGRES_DB >> $GITHUB_ENV
+echo $(ansible-vault view --vault-password-file=/vault_pass.txt env_secrets)|xargs -n1  >> $GITHUB_ENV
 rm /vault_pass.txt
