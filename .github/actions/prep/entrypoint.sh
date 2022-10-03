c0105fb0ee421cd6cdd94712d931bce10e683d18
Nadia Arias <nadia.arias@thoughtworks.com>
2022-10-04 08:47:30 +1000

    more testing

--- .github/actions/prep/entrypoint.sh
+++ .github/actions/prep/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo $VAULT_PASS > /vault_pass.txt
-$(ansible-vault view --vault-password-file=/vault_pass.txt env_secrets)|xargs -n1
+echo $(ansible-vault view --vault-password-file=/vault_pass.txt env_secrets)|xargs -n1
 echo "::add-mask::$POSTGRES_USER"
 echo "::add-mask::$POSTGRES_PASSWORD"
 echo "::add-mask::$POSTGRES_DB"
